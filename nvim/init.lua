1d84217a3a5023f9838d3fc197c2497342c7190c
Rex Fang <fh2ctm@gmail.com>
2024-07-26 23:15:18 -0400

    Small changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,8 @@
 local g = vim.g
+
 g.mapleader = ","
 g.maplocalleader = "\\"
+
 g.vimtex_view_method = 'skim'
 g.vimtex_quickfix_ignore_filters = { 'Overfull' }
 
@@ -16,6 +18,7 @@ if not vim.loop.fs_stat(mini_path) then
   vim.cmd('packadd mini.nvim | helptags ALL')
   vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
+
 require('mini.deps').setup { path = { package = path_package } }
 local now, later, add = MiniDeps.now, MiniDeps.later, MiniDeps.add
 
@@ -34,6 +37,7 @@ add { source = 'lervag/vimtex' }
 
 now(function() -- load now
   require('dark_notify').run()
+
   require('everforest').setup {
     background = 'hard',
     sign_column_background = 'grey',
@@ -41,8 +45,11 @@ now(function() -- load now
     diagnostic_line_highlight = true,
     inlay_hints_background = 'dimmed',
   }
+
   require('mini.starter').setup {}
+
   require('mini.statusline').setup {}
+
   require('mini.tabline').setup {}
 end)
 
@@ -50,9 +57,8 @@ now(function() -- vim
   vim.cmd('colorscheme everforest')
   vim.cmd('filetype plugin indent on')
   vim.cmd('syntax enable')
+
   local o = vim.opt
-  o.autoindent = true
-  o.colorcolumn = '81,101,121'
   o.cursorline = true
   o.expandtab = true
   o.fillchars = "fold: "
@@ -66,13 +72,14 @@ now(function() -- vim
   o.ignorecase = true
   o.inccommand = 'split'
   o.number = true
-  o.showmode = false
   o.shiftwidth = 2
+  o.showmode = false
   o.signcolumn = 'number'
-  o.tabstop = 2
   o.softtabstop = 2
   o.smartindent = true
   o.smarttab = true
+  o.smoothscroll = true
+  o.tabstop = 2
   o.termguicolors = true
 end)
 
@@ -88,7 +95,9 @@ end)
 
 later(function() -- load plugins later
   require('mini.ai').setup {}
+
   require('mini.bracketed').setup {}
+
   do -- mini.clue
     local mc = require('mini.clue')
     mc.setup {
@@ -100,9 +109,11 @@ later(function() -- load plugins later
         mc.gen_clues.registers(),
         mc.gen_clues.windows(),
         mc.gen_clues.z(),
+
         -- Secondary groups
-        { mode = 'n', keys = '<Leader><Space>', desc = '+Pick' },
+        { mode = 'n', keys = '<Leader>p', desc = '+Pick' },
         { mode = 'n', keys = '<LocalLeader>l', desc = '+VimTex' },
+
         -- VimTex
         { mode = 'n', keys = '<LocalLeader>la', desc = 'Context menu' },
         { mode = 'n', keys = '<LocalLeader>lc', desc = 'Clean' },
@@ -115,7 +126,6 @@ later(function() -- load plugins later
         { mode = 'n', keys = '<LocalLeader>lk', desc = 'Stop' },
         { mode = 'n', keys = '<LocalLeader>lK', desc = 'Stop all' },
         { mode = 'n', keys = '<LocalLeader>ll', desc = 'Compile' },
-        { mode = 'x', keys = '<LocalLeader>lL', desc = 'Compile selected' },
         { mode = 'n', keys = '<LocalLeader>lm', desc = 'List imaps' },
         { mode = 'n', keys = '<LocalLeader>lo', desc = 'Compile output' },
         { mode = 'n', keys = '<LocalLeader>lq', desc = 'Log' },
@@ -126,38 +136,70 @@ later(function() -- load plugins later
         { mode = 'n', keys = '<LocalLeader>lv', desc = 'View' },
         { mode = 'n', keys = '<LocalLeader>lx', desc = 'Reload' },
         { mode = 'n', keys = '<LocalLeader>lX', desc = 'Reload state' },
+
+        { mode = 'x', keys = '<LocalLeader>la', desc = 'Context menu' },
+        { mode = 'x', keys = '<LocalLeader>lc', desc = 'Clean' },
+        { mode = 'x', keys = '<LocalLeader>lC', desc = 'Full clean' },
+        { mode = 'x', keys = '<LocalLeader>le', desc = 'Errors' },
+        { mode = 'x', keys = '<LocalLeader>lg', desc = 'Status' },
+        { mode = 'x', keys = '<LocalLeader>lG', desc = 'All status' },
+        { mode = 'x', keys = '<LocalLeader>li', desc = 'Info' },
+        { mode = 'x', keys = '<LocalLeader>lI', desc = 'Full info' },
+        { mode = 'x', keys = '<LocalLeader>lk', desc = 'Stop' },
+        { mode = 'x', keys = '<LocalLeader>lK', desc = 'Stop all' },
+        { mode = 'x', keys = '<LocalLeader>ll', desc = 'Compile' },
+        { mode = 'x', keys = '<LocalLeader>lL', desc = 'Compile selected' },
+        { mode = 'x', keys = '<LocalLeader>lm', desc = 'List imaps' },
+        { mode = 'x', keys = '<LocalLeader>lo', desc = 'Compile output' },
+        { mode = 'x', keys = '<LocalLeader>lq', desc = 'Log' },
+        { mode = 'x', keys = '<LocalLeader>lr', desc = 'Reverse search' },
+        { mode = 'x', keys = '<LocalLeader>ls', desc = 'Toggle main' },
+        { mode = 'x', keys = '<LocalLeader>lt', desc = 'Open ToC' },
+        { mode = 'x', keys = '<LocalLeader>lT', desc = 'Toggle ToC' },
+        { mode = 'x', keys = '<LocalLeader>lv', desc = 'View' },
+        { mode = 'x', keys = '<LocalLeader>lx', desc = 'Reload' },
+        { mode = 'x', keys = '<LocalLeader>lX', desc = 'Reload state' },
       },
+
       triggers = {
         -- Leader
         { mode = 'n', keys = '<Leader>' },
         { mode = 'x', keys = '<Leader>' },
+
         -- Local Leader
         { mode = 'n', keys = '<LocalLeader>' },
         { mode = 'x', keys = '<LocalLeader>' },
+
         -- Built-in completion
         { mode = 'i', keys = '<C-x>' },
+
         -- `g` key
         { mode = 'n', keys = 'g' },
         { mode = 'x', keys = 'g' },
+
         -- Marks
         { mode = 'n', keys = "'" },
         { mode = 'n', keys = '`' },
         { mode = 'x', keys = "'" },
         { mode = 'x', keys = '`' },
+
         -- Registers
         { mode = 'n', keys = '"' },
         { mode = 'x', keys = '"' },
         { mode = 'i', keys = '<C-r>' },
         { mode = 'c', keys = '<C-r>' },
+
         -- `z` key
         { mode = 'n', keys = 'z' },
         { mode = 'x', keys = 'z' },
+
         -- Brackets
         { mode = 'n', keys = '[' },
         { mode = 'n', keys = ']' },
         { mode = 'x', keys = '[' },
         { mode = 'x', keys = ']' },
       },
+
       window = {
         config = {
           width = 'auto',
@@ -168,28 +210,46 @@ later(function() -- load plugins later
       },
     }
   end
+
   require('mini.comment').setup {
     mappings = {
       comment_line = '<leader>c',
       comment_visual = '<leader>c',
     },
   }
+
   require('mini.completion').setup {}
+
   require('mini.extra').setup {}
+
   require('mini.diff').setup {}
+
   require('mini.git').setup {}
+
   require('mini.icons').setup {}
+
   require('mini.jump').setup {}
+
   require('mini.jump2d').setup {}
+
   require('mini.files').setup {}
+
   require('mini.move').setup {}
+
   require('mini.pick').setup {}
+
+  require('mini.sessions').setup {
+    autoread = true,
+  }
+
   require('mini.splitjoin').setup {
     mappings = {
       toggle = '<Leader>m',
     },
   }
+
   require('mini.surround').setup {}
+
   require('treesitter-context').setup { enable = true }
 end)
 
@@ -202,9 +262,11 @@ later(function() -- leader key mappings
     { desc = desc }
     )
   end
+
   local lk = function(suffix, keycombo, desc) -- key sequence
     vim.keymap.set('n', '<Leader>' .. suffix, keycombo, { desc = desc })
   end
+
   lc('!', 'q!', 'Quit without saving')
   lc('b', 'split', 'Open buffer')
   lc('B', 'bd', 'Close buffer')
@@ -216,14 +278,15 @@ later(function() -- leader key mappings
   lc('S', 'w<CR>:source', 'Save and source')
   lc('t', 'tabnew', 'Open tab')
   lc('T', 'tabclose', 'Close tab')
-  lc('<Space>b', 'Pick buffers', 'Buffers')
-  lc('<Space>d', 'Pick diagnostic', 'Diagnostic')
-  lc('<Space>f', 'Pick files', 'Files')
-  lc('<Space>h', 'Pick help', 'Help')
-  lc('<Space>m', 'Pick marks', 'Marks')
-  lc('<Space>r', 'Pick registers', 'Registers')
   lk('v', '<C-w>t<CR><C-w>H', 'Split to vertical')
   lc('w', 'q', 'Quit')
+
+  lc('pb', 'Pick buffers', 'Buffers')
+  lc('pd', 'Pick diagnostic', 'Diagnostic')
+  lc('pf', 'Pick files', 'Files')
+  lc('ph', 'Pick help', 'Help')
+  lc('pm', 'Pick marks', 'Marks')
+  lc('pr', 'Pick registers', 'Registers')
 end)
 
 later(function() -- local leader key mappings
@@ -235,6 +298,7 @@ later(function() -- local leader key mappings
     { desc = desc }
     )
   end
+
   ll('[', 'LspStart', 'Start client')
   ll(']', 'LspStop', 'Stop client')
   ll('d', 'Pick diagnostic', 'Diagnostic')
